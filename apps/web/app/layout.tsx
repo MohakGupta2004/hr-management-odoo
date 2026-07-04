6eec7e79e18a4be5d3c4fe77422982cbece5ae8d
MOHAK GUPTA <mohakgupta@MOHAKs-MacBook-Air.local>
2026-07-04 10:09:39 +0530

    init: frontend add

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -1,14 +1,18 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
+import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import {ThemeProvider as NextThemeProvider} from 'next-themes'
+const inter = Inter({subsets:['latin'],variable:'--font-sans'});
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
+const geistSans = Geist({
   variable: "--font-geist-sans",
+  subsets: ["latin"],
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
+
+const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
+  subsets: ["latin"],
 });
 
 export const metadata: Metadata = {
@@ -22,10 +26,20 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+    <html
+      lang="en"
+      className={cn("h-full", "antialiased", geistSans.variable, geistMono.variable, "font-sans", inter.variable)}
+    >
+      <body className="min-h-full flex flex-col">
+      <NextThemeProvider
+attribute="class"
+defaultTheme="dark"
+enableSystem
+>
+{children}
+</NextThemeProvider>
+
+</body>
     </html>
   );
 }
